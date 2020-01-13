8fc76de3aae0ccb4bea5b04b2b81b9833e70204f
tincochan <mytinco@qq.com>
2020-01-13 10:02:14 +0800

    个人实现的微服务简单案例

--- single-spa.config.js
+++ single-spa.config.js
@@ -14,19 +14,22 @@ registerApplication(
     'home',
     // loadingFunction,这里加载相应的组件（生命周期）
     () => import('./src/home/home.app.js'),
-
-    'navBar',
-    () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
-    () => true,
-
-    'angularJS', 
-    () => import ('./src/angularJS/angularJS.app.js'), 
-    pathPrefix('/angularJS'),
-
     // activityFunction
     (location) => location.pathname === "" ||
     location.pathname === "/" ||
     location.pathname.startsWith('/home'),
 );
+
+registerApplication(
+    'navBar',
+    () => import('./src/navBar/navBar.app.js').then(module => module.navBar),
+    () => true,
+);
+
+registerApplication(
+    'angularJS', 
+    () => import ('./src/angularJS/angularJS.app.js'), 
+    pathPrefix('/angularJS'),    
+)
 
 start();
